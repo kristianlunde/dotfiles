27477be901bd63c24732e7b7fe305ebbc4a9176c
kristianlunde <kr.lunde@gmail.com>
2025-05-27 20:06:13 +0200

    added dadbod to nvim also added a very basic hyprland config

---
+++ .config/nvim/lua/plugins/dadbod.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "tpope/vim-dadbod",
+    lazy = true,
+  },
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = { "tpope/vim-dadbod" },
+    cmd = { "DBUI", "DBUIToggle", "DBUIFindBuffer", "DBUIRenameBuffer" },
+    init = function()
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+  },
+  {
+    "kristijanhusak/vim-dadbod-completion",
+    ft = { "sql", "mysql", "plsql" },
+    dependencies = { "tpope/vim-dadbod" },
+  },
+}
