33a8b544306548b59b20962faa8747f2a5c476f2
kristianlunde <kr.lunde@gmail.com>
2025-09-18 09:18:43 +0200

    feat: upgraded lsp config to neovim 0.11 syntax feat: remapped compose to super in keyd

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,54 +1,50 @@
+-- ~/.config/nvim/lua/plugins/lsp-config.lua
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			auto_install = true,
-		},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "mason-org/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    lazy = false,
+    dependencies = { "neovim/nvim-lspconfig" },
+    opts = {
+      auto_install = true,
+      ensure_installed = { "lua_ls", "ts_ls", "html", "solargraph", "intelephense" },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
+      -- define configs (no .setup)
+      vim.lsp.config("ts_ls", { capabilities = capabilities })
+      vim.lsp.config("html", { capabilities = capabilities })
+      vim.lsp.config("lua_ls", {
+        capabilities = capabilities,
+        settings = { Lua = { diagnostics = { globals = { "vim" } } } },
+      })
+      vim.lsp.config("intelephense", { capabilities = capabilities })
 
-			lspconfig.ts_ls.setup({
-				capabilities = capabilities,
-			})
+      -- enable servers
+      for _, name in ipairs({ "ts_ls", "solargraph", "html", "lua_ls", "intelephense" }) do
+        vim.lsp.enable(name)
+      end
 
-			lspconfig.solargraph.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.html.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.intelephense.setup({
-				capabilities = capabilities,
-			})
-
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
- 			vim.keymap.set("n", "<leader>gc", ":cclose<CR>", { desc = "Close quickfix list" })
-
-      -- See diagnostic messages
-      vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, {});
-      vim.keymap.set("n", "<leader>dl", vim.diagnostic.setloclist, {});
-
-
-		end,
-	},
+      -- keymaps
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>gc", ":cclose<CR>", { desc = "Close quickfix list" })
+      vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, {})
+      vim.keymap.set("n", "<leader>dl", vim.diagnostic.setloclist, {})
+    end,
+  },
 }
+
